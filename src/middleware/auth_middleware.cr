319eacfec6c63b546d5ff0892ddfcb717e948e7e
Evtimiy Mihaylov <evo@vaupe.com>
2020-07-13 20:32:57 +0300

    JWT token header updated

--- src/middleware/auth_middleware.cr
+++ src/middleware/auth_middleware.cr
@@ -4,15 +4,25 @@ module BankApp
     exclude ["/api/v1/login"], "POST"
     exclude ["/*"], "OPTIONS"
 
+    def get_token_from_request(env)
+      if !env.request.headers["Authorization"]?
+        raise "Authentication failed"
+      end
+
+      token_parts = env.request.headers["Authorization"].split("Bearer ")
+
+      if token_parts.size < 2
+        raise "Invalid Authorization Header"
+      end
+
+      return token_parts[1]
+    end
+
     def call(env)
       return call_next(env) if exclude_match?(env)
 
       begin
-        if !env.request.headers["Token"]?
-          raise "Authentication failed"
-        end
-
-        token = env.request.headers["Token"]
+        token = get_token_from_request(env)
 
         payload, header = JWT.decode(token, BankApp::SECRET_KEY, JWT::Algorithm::HS256)
         user_id = payload["user_id"].to_s
