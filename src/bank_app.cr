07fcd780a1a42d483e807589da796989516fc33b
Evtimiy Mihaylov <evo@vaupe.com>
2020-07-13 18:01:09 +0300

    Ability to add artificial delay for testing.

--- src/bank_app.cr
+++ src/bank_app.cr
@@ -12,11 +12,18 @@ module BankApp
   # JWT expiration timeout
   TOKEN_EXPIRATION_TIMEOUT           = (5 * 60)
   TOGGLE_AUTH_USER_TRANSACTIONS_ONLY = false
+  # Used for testing purposes
+  # example - 300ms: TOGGLE_ARTFICIAL_DELAY = 0.3
+  TOGGLE_ARTFICIAL_DELAY = 0
 
   Kemal.config.env = "production"
   Kemal.config.powered_by_header = false
 
   before_all "/api/v1/*" do |env|
+    if TOGGLE_ARTFICIAL_DELAY
+      sleep TOGGLE_ARTFICIAL_DELAY
+    end
+
     env.response.content_type = "application/json"
   end
 
